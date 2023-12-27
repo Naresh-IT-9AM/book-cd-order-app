e2f3f17c71192267f20eba9fc13e2e4ef238b171
aiworklabs <149414212+aiworklabs@users.noreply.github.com>
2023-12-27 20:40:11 +0530

    Create ServletInitializer.java

---
+++ src/main/java/com/yourapp/ServletInitializer.java
@@ -0,0 +1,12 @@
+package com.yourapp;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BookCdOrderApplication.class);
+    }
+}
