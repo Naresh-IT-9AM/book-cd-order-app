d33925f7b6b71ef29381e23e4b35c5868b50849a
aiworklabs <149414212+aiworklabs@users.noreply.github.com>
2023-12-27 20:27:06 +0530

    Create PaymentService.java

---
+++ src/main/java/com/yourapp/service/PaymentService.java
@@ -0,0 +1,11 @@
+package com.yourapp.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+    public boolean processPayment() {
+        // Implement payment processing
+        return true; // return true if payment is successful
+    }
+}
