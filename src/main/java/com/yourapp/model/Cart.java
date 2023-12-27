7d4a532cf433312669449dcc90f70ff84aff6196
aiworklabs <149414212+aiworklabs@users.noreply.github.com>
2023-12-27 20:25:14 +0530

    Create Cart.java

---
+++ src/main/java/com/yourapp/model/Cart.java
@@ -0,0 +1,10 @@
+package com.yourapp.model;
+
+import java.util.List;
+
+public class Cart {
+    private List<Book> books;
+    private List<CD> cds;
+
+    // Add methods to add/remove items, calculate total, etc.
+}
