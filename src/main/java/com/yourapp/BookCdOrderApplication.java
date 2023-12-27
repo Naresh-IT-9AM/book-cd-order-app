3a5ff4db398e2dbf1ccebab6119be8ce63679909
aiworklabs <149414212+aiworklabs@users.noreply.github.com>
2023-12-27 20:21:09 +0530

    Create BookCdOrderApplication.java

---
+++ src/main/java/com/yourapp/BookCdOrderApplication.java
@@ -0,0 +1,11 @@
+package com.yourapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookCdOrderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BookCdOrderApplication.class, args);
+    }
+}
